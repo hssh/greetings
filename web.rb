9b71f4000e783517ab23bfb5fdec64983479ff9a
hssh <hssh@hssh.jp>
2012-12-31 03:41:57 +0900

    Add compass support

--- web.rb
+++ web.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+  # set :haml, {format: :html5, escape_html: true}
+  set :haml, {format: :html5}
+  set :sass, Compass.sass_engine_options
+end
 
 get '/' do
   haml :index
 end
 
-get '/css/style.css' do
-  sass :'css/style'
+get '/css/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"css/#{params[:name]}", Compass.sass_engine_options
 end
